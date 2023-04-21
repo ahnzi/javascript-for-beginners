7173714256dbd00ef92cc098da198c7edef1377e
Ahnzi <jhhh0627@gmail.com>
2023-04-21 11:28:57 +0900

    ~#3.5 More Events

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-const a = 5;
-const b = 2;
+const h1 = document.querySelector("div.hello:first-child h1");
 
-let myName = "ahnzi";
+function handleTitleClick() {
+    h1.style.color = "blue";
+}
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+// 마우스가 title 위에 올라갈 때 발생하는 이벤트: mouseenter
+function handleMouseEnter() {
+    h1.innerTexth = "Mouse is here!";
+    console.log("mouse is here!");
+}
 
-myName = "nicolas";
+// 마우스가 title 위에서 벗어났을 때 발생하는 이벤트 : mouseleave
+function handleMouseLeave() {
+    h1.innerText = "Mouse is leave!";
+    console.log("mouse is leave!");
+}
 
-console.log("your new name is " + myName);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WiFi");
+}
+
+h1.addEventListener("click", handleTitleClick);
+// title.onclick = handleTitleClick;
+
+h1.addEventListener("mouseenter", handleMouseEnter);
+// title.onmouseenter = handleMouseEnter;
+
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
